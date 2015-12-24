325f11e9905067a1f022ebc2ffa23e1097421873
Vivek Sampara <ravensnowbird@gmail.com>
2015-12-24 13:30:11 +0530

    Added home and root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  root :to => 'home#index'
+
   resources :messages
   resources :rooms
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Serve websocket cable requests in-process
-  # mount ActionCable.server => '/cable'
+  mount ActionCable.server => '/cable'
 end
